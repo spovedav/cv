4b243f61a6f227d2eed64d68220159a9bd74b6e1
STEVYN POVEDA <37716320+spovedav@users.noreply.github.com>
2020-01-30 18:20:10 -0500

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -40,6 +40,7 @@ function proyectos3(){
             if (cont===1){
                 template+= `
                 <div class="carousel-item active">
+                     <button type="button"  data-toggle="modal" data-target="#exampleModal" value="video/video.mp4" title="reproducir video `+titulo+`" onclick="madal_video('`+titulo+`','`+datos[row]['url_video']+`');"><img src="images/video.png" alt="img de reproducir video `+datos[row]['url']+` " class="video"></button>
                     <img src="`+datos[row]['imagen']+`" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block" style="background-color: #000000ab !important;">
                         <h5>`+titulo+`</h5>
@@ -52,6 +53,7 @@ function proyectos3(){
             }else {
                 template+= `
                 <div class="carousel-item">
+                    <button type="button"  data-toggle="modal" data-target="#exampleModal" value="video/video.mp4" title="reproducir video `+titulo+`" onclick="madal_video('`+titulo+`','`+datos[row]['url_video']+`');"><img src="images/video.png" alt="img de reproducir video `+datos[row]['url']+` " class="video"></button>
                     <img src="`+datos[row]['imagen']+`" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block" style="background-color: #000000ab !important;">
                         <h5>`+titulo+`</h5>
@@ -84,6 +86,24 @@ function proyectos3(){
     });
 }
 
+
+/////////////  MODAL DE VIDEO  ////////////////////////
+function madal_video(titulo,ruta_video){
+    var template_video = '';
+    template_video = `
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">`+titulo+`</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                </div>
+                <div class="modal-body">
+                    <video src="video/video.mp4" controls width="100%" height="auto" autoplay></video>
+                </div>
+                `;
+              
+    document.getElementById('contenido_video').innerHTML = template_video;
+}
 
 
 ////////////   NIEVE  ////////////////
